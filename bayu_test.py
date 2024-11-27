15ed2615615adb65db623dfa9658d57a3358d5fc
Bayu <118286302+urDrek0@users.noreply.github.com>
2024-11-27 16:54:54 +0700

    Update bayu_test.py

--- bayu_test.py
+++ bayu_test.py
@@ -1,1 +1,11 @@
-print("Hello World")
+import pandas as pd
+
+# Create a dictionary with sample data
+data = {'name': ['Bayu Setyo Prajuritno', 'Muhammad Harits', 'Khoerul Umam', 'Muhammad Affandi Harahap'],
+        'nim': [1103223229, 1103223159, 1103223115, 1103223023]}
+
+# Create a pandas DataFrame
+df = pd.DataFrame(data)
+
+# Print the DataFrame
+df

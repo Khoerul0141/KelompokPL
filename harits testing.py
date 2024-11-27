a60efb3ff4db589afcf315204d20e5b584c7cd1a
kumisluhutgeming <114931478+kumisluhutgeming@users.noreply.github.com>
2024-11-27 16:53:58 +0700

    Add files via upload

---
+++ harits testing.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+data = {
+    "Nama": ["Muhammad Afandi", "Khoerul Umam", "Muhamad Harits", "Bayu Setyo"],  # Tambahkan nama sesuai kebutuhan
+    "NIM": ["1103223023", "1103223115", "1103223159", "1103223229" ],  # Tambahkan NIM sesuai kebutuhan
+}
+df = pd.DataFrame(data)
+
+print(df)
